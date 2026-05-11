7eb673e5904883264abbf011f07705b5b0e57b28
Sebastian Bernal <sebasbeay25@gmail.com>
2026-05-11 06:46:31 -0500

    Ejercicio 2: Colecciones y LINQ con clase Tarea

--- TaskManager/Program.cs
+++ TaskManager/Program.cs
@@ -1,20 +1,43 @@
-﻿string nombre = "Juan Pérez";
-int edad = 24;
-string lenguajeFavorito = "C#";
-decimal experiencia = 0.5m;
-bool disponible = true;
+﻿List<Tarea> tareas = new List<Tarea>();
+tareas.Add(new Tarea {Id = 1, Titulo = "Lavar Ropa", Categoria = "Personal", Completada = false, FechaCreacion = DateTime.Now});
+tareas.Add(new Tarea {Id = 2, Titulo = "Revisar Proyecto", Categoria = "Trabajo", Completada = true, FechaCreacion = DateTime.Now});
+tareas.Add(new Tarea {Id = 3, Titulo = "Subir Proyecto", Categoria = "Trabajo", Completada = false, FechaCreacion = DateTime.Now});
+tareas.Add(new Tarea {Id = 4, Titulo = "Aprender Tipos de Variables", Categoria = "Estudio", Completada = true, FechaCreacion = DateTime.Now});
+tareas.Add(new Tarea {Id = 5, Titulo = "Aprender LINQ", Categoria = "Estudio", Completada = false, FechaCreacion = DateTime.Now});
+tareas.Add(new Tarea {Id = 6, Titulo = "Aprender Colecciones", Categoria = "Estudio", Completada = false, FechaCreacion = DateTime.Now});
 
-Console.WriteLine("╔══════════════════════════════════╗");
-Console.WriteLine($"    Nombre:          {nombre}");
-Console.WriteLine($"    Edad:            {edad} años");
-Console.WriteLine($"    Lenguaje:        {lenguajeFavorito}");
-Console.WriteLine($"    Experiencia:     {experiencia}");
-Console.WriteLine($"    Disponible:      {(disponible ? "Sí" : "No")}");
-Console.WriteLine("╚══════════════════════════════════╝");
+//Recorre todos los elementos y valida uno por uno si su categoría es estudio o no, si es verdadero, agrega el elemento a la nueva lista
+var tareasEstudio = tareas.Where(t => t.Categoria == "Estudio").ToList();
 
-// ¿Qué pasaría si cambias decimal por double para la experiencia?: En las pruebas que hice, muestra el valor exactamente igual para ambos escenarios, pero en el código, decimal usa m al final del valor y double no, entonces debo quitar o agregar la m dependiendo el tipo que escoja
+//Recorre todos los elementos de la lista verifica cada elemento si está completado o no por medio de la variable Completada, agrega los elementos que cumplen con la condición a la nueva lista y los ordena por fecha de creación
+var tareasNoCompletadas = tareas.Where(t => !t.Completada).OrderBy(t => t.FechaCreacion).ToList();
 
-double d = 0.1 + 0.2;
-decimal m = 0.1m + 0.2m;
-Console.WriteLine(d); // sorpresa
-Console.WriteLine(m); // exacto
+//Hace un contador interno y va aumentando su valor cada que encuentra una tarea completada
+int totalCompletadas = tareas.Count(t => t.Completada);
+
+//Recorre todos los elementos buscando la palabra C# en el titulo de cada una, si encuentra almenos uno, devuelve true
+bool tareaC = tareas.Any(t => t.Titulo.Contains("C#"));
+
+//Recorre todos los elementos y transforma a mayuscula cada titulo encontrado, el resultado lo guarda en una nueva lista
+var tareasMayusculas = tareas.Select(t => t.Titulo.ToUpper());
+
+Console.WriteLine("Tareas de Categoria Estudio: ");
+foreach (var tarea in tareasEstudio)
+    Console.WriteLine(tarea.Titulo);
+Console.WriteLine();
+
+Console.WriteLine("Tareas No Completadas");
+foreach (var tarea in tareasNoCompletadas)
+    Console.WriteLine(tarea.Titulo);
+Console.WriteLine();
+
+Console.WriteLine($"Total de Tareas Completadas: {totalCompletadas}");
+Console.WriteLine();
+
+Console.WriteLine($"¿Existe alguna tarea con la palabra C# en el título?: {(tareaC ? "Sí" : "No")}");
+Console.WriteLine();
+
+Console.WriteLine("Tareas en Mayúsculas:");
+foreach (var tarea in tareasMayusculas)
+    Console.WriteLine(tarea);
+Console.WriteLine();
--- TaskManager/obj/Debug/net10.0/TaskManager.AssemblyInfo.cs
+++ TaskManager/obj/Debug/net10.0/TaskManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddbb25d07871315b72d6093390159743b54d1bfa")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
